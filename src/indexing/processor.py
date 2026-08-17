59aec48ad17818deab700b227575704cf48f8a05
Mohamed Mamdouh <ai.mohamed.mamdouh@gmail.com>
2026-08-18 01:51:05 +0300

    fix: processor

--- src/indexing/processor.py
+++ src/indexing/processor.py
@@ -71,12 +71,16 @@ class DocumentProcessor :
     def document_processing_pipeline(self, path : str, vector_store:VectorStore ) :
         documents = DocumentLoader().load_data(PDFLoader(path=path))
         clean_documents = DocumentCleaner().clean_documents(documents=documents)
+
         sections = DocumentSplitter().split_documents_by_headings(clean_documents)
+
         blocks = DocumentSplitter().split_text_and_tables(sections)
+
         chunks = DocumentSplitter().split_documents_to_chunks(blocks)
+
         chunks = self.enrich_documents(chunks) 
         final_chunks = self.deduplicate_documents(chunks)
-        final_chunks = DocumentSplitter().add_chunk_indices(chunks)
+        final_chunks = DocumentSplitter().add_chunk_indices(final_chunks)
 
         ids = vector_store.generate_chunks_ids(final_chunks)
         ids = vector_store.add_new_documents(vector_store=vector_store.get_vector_store(), chunks=final_chunks, ids=ids)
