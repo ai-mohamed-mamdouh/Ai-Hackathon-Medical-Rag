fdec4abde8925a8bb10a331a81cc27675b39249e
Mohamed Mamdouh <ai.mohamed.mamdouh@gmail.com>
2026-08-18 17:42:09 +0300

    updata: setting/TOP_K

--- src/config/settings.py
+++ src/config/settings.py
@@ -21,7 +21,7 @@ class Settings:
 
     COLLECTION_NAME="medical_giddiness"
 
-    TOP_K=10
+    TOP_K=4
     RRF_K=60
     RERANKER_MODEL_NAME = "cross-encoder/ms-marco-MiniLM-L2-v2"  # "cross-encoder/ms-marco-MiniLM-L6-v2"
 
